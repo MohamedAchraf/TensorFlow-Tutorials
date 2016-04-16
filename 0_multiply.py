544b1f33ffc155af8e53a89512558d3d4e93ff8d
yosaikan <mohamedachraf@gmail.com>
2016-04-16 12:10:35 +0100

    first commit

--- 0_multiply.py
+++ 0_multiply.py
@@ -2,11 +2,12 @@
 
 import tensorflow as tf
 
-a = tf.placeholder("float") # Create a symbolic variable 'a'
-b = tf.placeholder("float") # Create a symbolic variable 'b'
+a = tf.placeholder(tf.int8) # Create a symbolic variable 'a'
+b = tf.placeholder(tf.int8) # Create a symbolic variable 'b'
 
 y = tf.mul(a, b) # multiply the symbolic variables
+# test comment
 
 with tf.Session() as sess: # create a session to evaluate the symbolic expressions
-    print "%f should equal 2.0" % sess.run(y, feed_dict={a: 1, b: 2}) # eval expressions with parameters for a and b
-    print "%f should equal 9.0" % sess.run(y, feed_dict={a: 3, b: 3})
+    print "%d should equal 2.0" % sess.run(y, feed_dict={a: 1, b: 2}) # eval expressions with parameters for a and b
+    print "%d should equal 9.0" % sess.run(y, feed_dict={a: 3, b: 3})
